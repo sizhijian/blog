1aafd38969340162bc03dec7030fe675a464aa44
sizhijian <sizhijian619@163.com>
2017-06-29 19:00:27 +0800

    commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,17 @@
   <div>
     <HeaderItem logined=logined></HeaderItem>
     <Tabs @on-click="handleClick" type="line">
-      <Tab-pane v-for="(item , index) in compiledMarkdown" :key="index" :label="item.type">
+      <Tab-pane v-for="(item , index) in compiledMarkdown" :key="index" :label="item.type" ref="tabsItem">
         <div class="container">
-          <Card v-for="(subitem , subindex) in item.contain" :key="subindex" :bordered="true">
+          <Card v-for="(subitem , subindex) in item.contain" :key="subindex" :bordered="true" :class="{active : subitem.packUp}">
             <p slot="title" style="text-align: center;font-size: 16px;">{{subitem.title}}</p>
             <div v-html="subitem.content"></div>
+            <!--{{subitem.packUp}}-->
+            {{subitem.initHeight}}
+            <a class="arrow" @click="handleToggle(index,subindex)">
+              <Icon v-if="subitem.packUp" type="ios-arrow-down"></Icon>
+              <Icon v-else type="ios-arrow-up"></Icon>
+            </a>
           </Card>
           <div style="height:10px;"></div>
         </div>
@@ -21,6 +27,11 @@
   .ivu-tabs-nav-container{width: 980px;margin: 0 auto;padding: 0 5px;}
   .ivu-tabs-tabpane .container{padding: 0 10px;}
   .ivu-card{margin-bottom: 10px;background: #f6f8fa}
+  .arrow{
+    position: absolute;
+    left:0;bottom: -6px;width: 100%;
+    text-align: center;font-size: 18px;}
+  .active{max-height: 200px;overflow: hidden}
 </style>
 <script>
   import HeaderItem from './Header'
@@ -40,26 +51,41 @@
     },
     data () {
       return {
-        diretory: []
+        directory: [],
+        arrow: false
       }
     },
     computed: {
       compiledMarkdown: function () {
-        return this.diretory.filter(function (item) {
-          return item.contain.filter(function (subitem) {
+        return this.directory.filter(function (item, index) {
+          return item.contain.filter(function (subitem, subindex) {
             subitem.content = marked(subitem.content);
+            var initHeight = document.getElementsByClassName("ivu-tabs-tab")[0]
+//            var initHeight = this.$refs.tabsItem
+
+            subitem.initHeight = initHeight;
             return item.contain
           });
           return item;
         })
       }
+
     },
     created() {
       this.$http.get(
         CONST_apiUrl + '/articles'
       ).then((response) => {
-        this.diretory = response.body.info;
-//        console.log(JSON.stringify(this.diretory));
+
+        response.body.info.forEach((item)=>{
+          item.contain.forEach((subitem)=>{
+            subitem.packUp = true;
+            subitem.initHeight = 0;
+//            console.log(JSON.stringify(subitem))
+          });
+
+        });
+        this.directory = response.body.info;
+//        console.log(JSON.stringify(this.directory));
       });
     },
     mounted(){
@@ -68,7 +94,11 @@
     methods: {
       handleClick(e){
           var el = document.getElementsByClassName("ivu-tabs-content")[0];
-        el.style.height = el.childNodes[e].firstChild.offsetHeight + "px";
+          el.style.height = el.childNodes[e].firstChild.offsetHeight + "px";
+      },
+      handleToggle(i,si){
+//        console.log(this.directory[i].contain[si].packUp);
+        this.directory[i].contain[si].packUp = !this.directory[i].contain[si].packUp;
       }
     }
   }
